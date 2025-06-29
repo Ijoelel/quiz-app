94a554a398533e11298caba4d1324de348dbbddc
Ijoelel <fianarnatha13@gmail.com>
2025-06-30 05:14:01 +0700

    fix image src

--- components/Card.vue
+++ components/Card.vue
@@ -13,7 +13,7 @@ const page = usePageStore();
         class="bg-transparent shadow-md rounded-lg p-4 border border-cream text-white flex flex-col items-center w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 transition-transform duration-300 hover:scale-105 hover:shadow-lg cursor-pointer"
     >
         <img
-            :src="`_nuxt/${image}`"
+            :src="`/${image}`"
             :alt="`${title} Image`"
             class="w-full h-52 object-cover rounded-lg mb-4"
         />
